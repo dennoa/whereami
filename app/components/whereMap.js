4695f6d936e18d575ef75b7ebcebef4f6587be23
Andrew Dennison <andrew.dennison@live.com.au>
2018-03-19 13:45:39 +1000

    get current location

--- app/components/whereMap.js
+++ app/components/whereMap.js
@@ -3,13 +3,23 @@ import PropTypes from 'prop-types'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 const WhereMap = withGoogleMap(props => (
-  <GoogleMap defaultZoom={15} defaultCenter={{ lat: -28.65, lng: 153.61 }}>
-    {props.isMarkerShown && <Marker position={{ lat: -29, lng: 153.5 }} />}
+  <GoogleMap defaultZoom={props.defaultZoom || 15} center={props.center}>
+    {(props.markers || []).map(marker => (
+      <Marker
+        key={marker.key}
+        position={marker.position}
+        label={marker.label}
+        icon={marker.icon}
+        title={marker.title}
+      />
+    ))}
   </GoogleMap>
 ))
 
 WhereMap.propTypes = {
-  isMarkerShown: PropTypes.bool,
+  center: PropTypes.object.isRequired,
+  defaultZoom: PropTypes.number,
+  markers: PropTypes.array,
 }
 
 export default WhereMap
--- app/views/home.js
+++ app/views/home.js
@@ -1,17 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import WhereMap from 'Components/whereMap'
 
-const Home = () => (
-  <div>
-    <div className="container">
-      <WhereMap
-        containerElement={<div style={{ height: '800px' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-        isMarkerShown={false}
-      />
-    </div>
-  </div>
-)
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      myPosition: { lat: 0, lng: 0 },
+      myName: 'Denno',
+    }
+    this.watchPositionId = undefined
+    this.handlePositionChange = this.handlePositionChange.bind(this)
+    this.handleError = this.handleError.bind(this)
+  }
+
+  componentWillMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.handlePositionChange, this.handleError)
+      this.watchPositionId = navigator.geolocation.watchPosition(this.handlePositionChange, this.handleError)
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.watchPositionId) {
+      navigator.geolocation.clearWatch(this.watchPositionId)
+    }
+  }
+
+  handlePositionChange(position) {
+    const { latitude, longitude } = position.coords
+    this.setState({ myPosition: { lat: latitude, lng: longitude } })
+  }
+
+  handleError(err) {
+    console.log(err)
+  }
+
+  render() {
+    const { myName, myPosition } = this.state
+
+    return (
+      <div>
+        <div className="container">
+          <WhereMap
+            containerElement={<div style={{ height: '800px' }} />}
+            mapElement={<div style={{ height: '100%' }} />}
+            center={myPosition}
+            markers={[
+              {
+                key: 'me',
+                position: myPosition,
+                label: myName[0],
+                title: myName,
+              },
+            ]}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Home
